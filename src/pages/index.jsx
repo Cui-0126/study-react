24ce812e00a40b0d7f3168b3b0503fd8992eee1e
Cui-0126 <lily410happy@gmail.com>
2024-11-09 19:38:24 +0900

    ✨  #13 useStateの文字列・真偽地について

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -18,7 +18,9 @@ const geistMono = localFont({
 });
 
 export default function Home() {
-  const [count, setCount] = useState(1)
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
   const handleClick = useCallback( 
     () => {
@@ -26,6 +28,18 @@ export default function Home() {
     setCount(count => count + 1);
     }
   }, [count]) ;
+
+  const handleDisplay = useCallback(() =>{
+    setIsShow((isShow) => !isShow);
+  }, []);
+
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5){
+      alert("5文字以内にしてください");
+      return;
+    }
+    setText(e.target.value.trim());
+ },[]);
 
   useEffect(() => {
      document.body.style.backgroundColor = "lightblue";
@@ -37,8 +51,12 @@ export default function Home() {
   return (
     <>
       <Headter></Headter>
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1>: null}
       <button onClick={handleClick} > ボタン </button>
+      <button onClick={handleDisplay} > {isShow ? "非表示" : "表示"} </button>
+
+      <input 
+      type="text" value={text} onChange={handleChange} />
 
       <Top />
 
