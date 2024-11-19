b92da1cc72764acb289f2abe5df9dc892fd70a47
Cui-0126 <lily410happy@gmail.com>
2024-11-19 09:19:52 +0900

    ✨ #17 useRouterについて

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,15 @@
-import "src/styles/globals.css";
 import type { AppProps } from "next/app";
+import "src/styles/globals.css";
+import { useBgColor } from "../hooks/useBgColor";
 import { useCounter } from "../hooks/useCounter";
 import { useInputArray } from "../hooks/useInputArray";
-import { useBgLightBlue } from "../hooks/useBgBlueLight";
 
 export default function App({ Component, pageProps }: AppProps) {
 
   const counter = useCounter ();
   console.log(counter)
   const inputArray = useInputArray ();
-  useBgLightBlue ();
+  useBgColor ();
 
   return <Component {...pageProps} {...counter} {...inputArray} />;
 }
